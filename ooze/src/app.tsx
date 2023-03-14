9a5af888f15b9c67fd8d0c73197f86fe74bd9828
ed_e <edxtdev@gmail.com>
2023-03-14 20:07:26 +0000

    get vite working on enwiki

--- ooze/src/app.tsx
+++ ooze/src/app.tsx
@@ -4,9 +4,13 @@ export function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div style={{
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+    }}>
       <p>Hello! State is {count}</p>
       <button onClick={() => setCount(count + 1)}>Increment</button>
-    </>
+    </div>
   )
 }
--- ooze/src/main.tsx
+++ ooze/src/main.tsx
@@ -1,4 +1,10 @@
 import { render } from 'preact'
 import { App } from './app'
 
-render(<App />, document.getElementById('app') as HTMLElement)
+// Create a new element to render into
+const oozeContainer = document.createElement("div");
+oozeContainer.id = "ooze-container";
+document.body.appendChild(oozeContainer);
+
+// Render the app into the new element
+render(<App />, oozeContainer);
